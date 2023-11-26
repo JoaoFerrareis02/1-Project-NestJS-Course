5bc634d773ea270e691d099f683a0b7ac191608b
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2023-11-26 12:31:07 -0300

    Trabalhando com TypeORM e SQLite

---
+++ src/database/database.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Course } from 'src/courses/entities/courses.entity';
+import { Tag } from 'src/courses/entities/tags.entity';
+import { DataSourceOptions } from 'typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'sqlite',
+  database: 'db.sqlite',
+  synchronize: true,
+  entities: [Course, Tag],
+};
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        return {
+          ...dataSourceOptions,
+        };
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
