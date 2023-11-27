5bba720e8fd55a5782f32eaa9b54f5b6a3e638f3
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2023-11-27 09:57:34 -0300

    Configurações Globais no NestJS

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,24 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from 'src/courses/entities/courses.entity';
 import { Tag } from 'src/courses/entities/tags.entity';
-import { DataSourceOptions } from 'typeorm';
-
-export const dataSourceOptions: DataSourceOptions = {
-  type: 'sqlite',
-  database: 'db.sqlite',
-  synchronize: false,
-  entities: [Course, Tag],
-};
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: () => {
+      useFactory: (configService: ConfigService) => {
         return {
-          ...dataSourceOptions,
+          type: 'sqlite',
+          database: configService.get('DB_NAME'),
+          synchronize: false,
+          entities: [Course, Tag],
         };
       },
+      inject: [ConfigService],
     }),
   ],
 })
--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,10 +1,19 @@
-import { DataSource } from 'typeorm';
-import { dataSourceOptions } from './database.module';
+import 'dotenv/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { CreateCoursesTable1701021017258 } from 'src/migrations/1701021017258-CreateCoursesTable';
 import { CreateTagsTable1701022883874 } from 'src/migrations/1701022883874-CreateTagsTable';
 import { CreateCoursesTagsTable1701047944478 } from 'src/migrations/1701047944478-CreateCoursesTagsTable';
 import { AddCoursesIdToCoursesTagsTable1701078892955 } from 'src/migrations/1701078892955-AddCoursesIdToCoursesTagsTable';
 import { AddTagsIdToCoursesTagsTable1701079491690 } from 'src/migrations/1701079491690-AddTagsIdToCoursesTagsTable';
+import { Course } from 'src/courses/entities/courses.entity';
+import { Tag } from 'src/courses/entities/tags.entity';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'sqlite',
+  database: process.env.DB_NAME,
+  synchronize: false,
+  entities: [Course, Tag],
+};
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
